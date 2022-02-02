c0a1bdfb763b08f9fecae2e9bbac1ffdc56a388e
Karyna Kananovich <karyna.kananovich@gmail.com>
2022-02-02 10:44:35 +0300

    feat: add type guardian

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
-    const { length } = this.collection;
+    const { length } = this.collection
 
     for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i -1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHand = this.collection[j]
+            this.collection[j] = this.collection[j + 1]
+            this.collection[j + 1] = leftHand
+          }
         }
       }
     }
   }
 }
 
-const sorter = new Sorter([4, -3, 11, 2]);
-sorter.sort();
-console.log(sorter.collection);
+const sorter = new Sorter([4, -3, 11, 2])
+sorter.sort()
+console.log(sorter.collection)
