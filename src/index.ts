537e0b5fcb42cee828ceee3a0fdd76b926349d43
Karyna Kananovich <karyna.kananovich@gmail.com>
2022-02-02 16:24:14 +0300

    refactor: make the Sorter class abstract and remove the interface

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-import { Sorter } from './Sorter'
+import { NumbersCollection } from './NumbersCollection'
+import { CharacterCollection } from './CharactersCollection'
+import { LinkedList } from './LinkedList'
 
-const sorter = new Sorter([4, -3, 11, 2])
-sorter.sort()
-console.log(sorter.collection)
+const numbersCollection = new NumbersCollection([4, -3, 110, 2])
+numbersCollection.sort()
+console.log(numbersCollection.data)
+
+const charactersCollection = new CharacterCollection('ZxYmba')
+charactersCollection.sort()
+console.log(charactersCollection.data)
+
+const linkedList = new LinkedList()
+linkedList.add(100)
+linkedList.add(-11)
+linkedList.add(3)
+linkedList.add(7)
+linkedList.sort()
+linkedList.print()
